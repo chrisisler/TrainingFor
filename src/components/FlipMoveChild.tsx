1ff67532e77c8302a1304729259e9f0230dea8d1
chrisisler <christopherisler1@gmail.com>
2021-03-22 12:34:52 -0400

    Move ActivityView to its own file

---
+++ src/components/FlipMoveChild.tsx
@@ -0,0 +1,6 @@
+import React, { forwardRef, ReactNode } from 'react';
+
+export const FlipMoveChild = forwardRef<
+  HTMLDivElement,
+  { children: ReactNode }
+>((props, ref) => <div ref={ref}>{props.children}</div>);
