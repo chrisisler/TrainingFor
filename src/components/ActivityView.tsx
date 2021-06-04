3d928d5f70e59ab405f6430e2ef1068276626a23
chrisisler <christopherisler1@gmail.com>
2021-06-04 19:58:51 -0400

    Remove AddSet box shadow

--- src/components/ActivityView.tsx
+++ src/components/ActivityView.tsx
@@ -271,7 +271,6 @@ export const ActivityView = forwardRef<
               height: min-content;
               padding: ${Pad.XSmall};
               margin-right: ${Pad.Small};
-              box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
               outline: none;
             `}
           >
